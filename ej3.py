42fee4d143be138b94c3a4c745068014afc6ebce
Dano19186 <92856939+Dano19186@users.noreply.github.com>
2023-04-04 18:03:31 +0200

    ej3

--- ej3.py
+++ ej3.py
@@ -1,12 +1,9 @@
 import re
 
 entrada = input()
-patron = "\\b\d{4}-\d{2}-\d{2}\\b"
-patron2 = "\d{2,4}"
+patron = "\\b(\\d{4})-(\\d{2})-(\\d{2})\\b"
 r = re.findall(patron, entrada)
-l = " ".join(map(str, r))
-separados = re.findall(patron2, l)
-separados.reverse()
-lsol = ".".join(map(str, separados))
-sol = re.sub(patron, lsol, entrada)
+year, mes, day = r[0][0], r[0][1], r[0][2]
+invertido = day+"."+mes+"."+year
+sol = re.sub(patron, invertido, entrada)
 print(sol)
