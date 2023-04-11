de6539a6cef8ca62ad2d934efac2d91e740719e9
servgo <sergioveguegonzalez@gmail.com>
2023-04-12 00:28:13 +0200

    nuevo

--- ej6.py
+++ ej6.py
@@ -1,33 +1,30 @@
+import csv
 import re
-
-
-def print_csv(csv_file):
-    with open(csv_file, 'r') as file:
-        csv_reader = csv.reader(file)
-        for row in csv_reader:
-            print(row)
-
 
 regex = r'^(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d+)\s+(\S+)\s+(\S+)\s+---\s+\[(\S+)\]\s+(\S+)\s+:\s+(.*)$'
 
-csv = 'logs.csv'
+csv_file = 'logs.csv'
 
 logs = []
 log = input()
 logs.append(log)
+match = re.match(regex, log)
 
-with open(csv, 'w') as csv_file:
-    if csv_file.tell() == 0:
-        csv_file.write('"Nivel de log","Hilo","Clase responsable","Mensaje de log"\n')
-
+with open(csv_file, 'w') as file:
     for log in logs:
-        match = re.match(regex, log)
         if match:
             nivelLog = match.group(2)
             hilo = match.group(4)
-            claseResponsable = match.group(5).split('.')[0]
+            claseResponsable = match.group(5).split('.')[1]
             mensaje = match.group(6)
 
-            csv_file.write(f'"{nivelLog}","{hilo}","{claseResponsable}","{mensaje}"\n')
+            file.write(f'"{nivelLog}","{hilo}","{claseResponsable}","{mensaje}"\n')
+        else:
+            print(None)
 
-print_csv('logs.csv')
+file.close()
+
+with open(csv_file, 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print('"'+row[0]+'",''"'+row[1]+'",''"'+row[2]+'",''"'+row[3]+'"')
