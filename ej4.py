a908adefe99207ec14d894210ec4cf31f51d678a
Dano19186 <92856939+Dano19186@users.noreply.github.com>
2023-04-11 16:17:44 +0200

    ej4

--- ej4.py
+++ ej4.py
@@ -1,12 +1,12 @@
 import re
 
 entrada = input()
-patron = "\\b([a-zA-Z]+)\\.([a-zA-Z]{2,})(\\.([0-9]{4})@alumnos.urjc.es|@urjc.es)\\b"
+patron = r"\b([a-zA-Z]{1}\.([a-zA-Z]{2,})\.(\d{4})(@alumnos\.urjc\.es))|(([a-zA-Z]+)\.([a-zA-Z]+)@urjc\.es)\b"
 r = re.findall(patron, entrada)
 i = 0
 while i < len(r):
-    if r[i][2] == "@urjc.es":
-        print("profesor "+r[i][0]+" apellido "+r[i][1])
+    if r[i][3] != "@alumnos.urjc.es":
+        print("profesor "+r[i][5]+" apellido "+r[i][6])
     else:
-        print("alumno "+r[i][1]+" matriculado en "+r[i][3])
+        print("alumno "+r[i][1]+" matriculado en "+r[i][2])
     i += 1
