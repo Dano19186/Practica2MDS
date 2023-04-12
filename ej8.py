4b515f62a00257cf54207fd283985bef99332b5d
Dano19186 <92856939+Dano19186@users.noreply.github.com>
2023-04-12 19:22:32 +0200

    ej7

--- ej8.py
+++ ej8.py
@@ -1,6 +1,10 @@
 import re
 
+"""regex = r".* +([A-Z]+).* --- \[(.*)\] .*\.?([A-Z]\w+) *: (.*)"""
 regex = r'\b\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d+\s+(\S+)\s+\S+\s+---\s+\[(\S+)\]\s+[a-z.]*([A-Z]\S+)\s+:\s+(.*)'
+
+
+
 
 log = input()
 match = re.match(regex, log)
