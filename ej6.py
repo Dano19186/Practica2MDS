b1c62d654c2609443c636e3f3ead9119c83ed9b3
servgo <sergioveguegonzalez@gmail.com>
2023-04-12 17:37:59 +0200

    nuevo

--- ej6.py
+++ ej6.py
@@ -1,30 +1,24 @@
-import csv
 import re
 
-regex = r'^(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d+)\s+(\S+)\s+(\S+)\s+---\s+\[(\S+)\]\s+(\S+)\s+:\s+(.*)$'
+regex = r'\b\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d+\s+(\S+)\s+\S+\s+---\s+\[(\S+)\]\s+[a-z.]*([A-Z]\S+)\s+:\s+(.*)'
 
 csv_file = 'logs.csv'
 
-logs = []
 log = input()
-logs.append(log)
 match = re.match(regex, log)
 
 with open(csv_file, 'w') as file:
-    for log in logs:
-        if match:
-            nivelLog = match.group(2)
-            hilo = match.group(4)
-            claseResponsable = match.group(5).split('.')[1]
-            mensaje = match.group(6)
+    if match:
+        nivelLog = match.group(1)
+        hilo = match.group(2)
+        claseResponsable = match.group(3)
+        mensaje = match.group(4)
 
-            file.write(f'"{nivelLog}","{hilo}","{claseResponsable}","{mensaje}"\n')
-        else:
-            print(None)
+        file.write(f'"{nivelLog}","{hilo}","{claseResponsable}","{mensaje}"\n')
 
 file.close()
 
-with open(csv_file, 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        print('"'+row[0]+'",''"'+row[1]+'",''"'+row[2]+'",''"'+row[3].replace(',',' ')+'"')
+if match:
+    with open(csv_file, 'r') as file:
+        for line in file:
+            print(line)
---
+++ ej8.py
@@ -0,0 +1,18 @@
+import re
+
+regex = r'\b\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d+\s+(\S+)\s+\S+\s+---\s+\[(\S+)\]\s+[a-z.]*([A-Z]\S+)\s+:\s+(.*)'
+
+log = input()
+match = re.match(regex, log)
+
+if match:
+    nivelLog = match.group(1)
+    hilo = match.group(2)
+    claseResponsable = match.group(3)
+    mensaje = match.group(4)
+
+    line = '"'+nivelLog+'"'+',"'+hilo+'"'+',"'+claseResponsable+'",'+mensaje+'"'
+    print(line)
+
+else:
+    print(None)
